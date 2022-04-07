7d4ee15a686e21e2285c6d75ff27f5cbb5445a12
mariotawfik <amxmaroz@gmail.com>
2022-04-07 10:29:10 +0200

    Initial Commit

---
+++ Q&S/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Q&S
+//
+//  Created by Mary Tamry on 07.04.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
